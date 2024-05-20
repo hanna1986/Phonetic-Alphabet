cf79a8144ef85b8c9d611ba37f3a98e1792e0022
hanna1986 <54247496+hanna1986@users.noreply.github.com>
2024-05-20 02:49:03 -0500

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,12 +1,13 @@
 class Alphabet:
 
-    def covert(name: str, dictionary: dict):
+    def convert(name: str, dictionary: dict):
         #from alphabet_dict import Alphabet_dict
         #alpha = Alphabet_dict.alpha
 
         #name = "Yuriy Indman"
+        name = name.strip()
+        name = name.replace("\n", " ")
         output=""
-        
         for letter in name:
             if letter == " ":
                 output = output + "space\n"
